8143d98a0a5b337de372b6bea09a22eeea1c1c89
Tien <doxuantien96@gmail.com>
2022-07-20 23:46:20 +0700

    fix bug

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,7 +1,92 @@
-import React from 'react';
-const Portfolio = () => {
+import React, { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+import Portfolios from '../../data/portfolio';
+
+const Portfolio = () => { 
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const [portfolio, setPortfolio] = useState<{
+        cover: string;
+        title: string;
+        description: string[];
+        url: string;
+    }[]>([]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    });
+
+    useEffect(() => {
+        getPortfolio();
+    }, []);
+
+    const getPortfolio = async () => {
+        setPortfolio(Portfolios.portfolio);
+    };
+
+    console.log(portfolio);
+
+    const renderPortfolio = (portfolio: {
+        cover: string;
+        title: string;
+        description: string[];
+        url: string;
+    }[]) => {
+        return (
+            <div className="images-container">
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className="image-box" key={idx}>
+                                <img 
+                                    src={port.cover}
+                                    className="portfolio-image"
+                                    alt="portfolio" />
+                                <div className="content">
+                                    <p className="title">{port.title}</p>
+                                    <h4 className="description">
+                                        {
+                                            port.description.map((value, index) => {
+                                                return  <span key={index}>{value}<br/></span>;
+                                            }) 
+                                        }
+                                    </h4>
+                                    {/* <h4 className="description">{port.description}</h4> */}
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        );
+    };
+
+
     return (
-        <></>
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={'Portfolio'.split('')}
+                        idx={15}
+                    />
+                </h1>
+                <div>{renderPortfolio(portfolio)}</div>
+            </div>
+            <Loader active type="pacman" />
+        </>
     );
 };
 
---
+++ src/data/portfolio.ts
@@ -0,0 +1,46 @@
+import spinnee from '../assets/images/portfolio/spinnee.webp';
+import mibitel from '../assets/images/portfolio/mibitel.webp';
+import apec_finance from '../assets/images/portfolio/apec_finance.webp';
+import grandit from '../assets/images/portfolio/grandit.png';
+import mvs from '../assets/images/portfolio/mvs.jpg';
+
+export default {
+    'portfolio': [
+        {
+            'cover': spinnee,
+            'title': 'SPINEE',
+            'description': ['Personal project', 'Java android, Android studio, Retrofit, Git', 'Design pattern: Singleton, Observer', 'Structure: MVP, MVVM'],
+            'url': 'https://play.google.com/store/apps/details?id=com.lotustar.penmodapp&hl=vi&gl=US'
+        },
+        {
+            'cover': grandit,
+            'title': 'GRANDIT',
+            'description': ['Customer: GrandIT Japan', 'C#, .NET, GIT, SVN', 'SQL server: Microsoft SQL server', 'Cloud: AWS'],
+            'url': 'https://www.grandit.jp/'
+        },
+        {
+            'cover': mvs,
+            'title': 'My Viettel store',
+            'description': ['Customer: Viettel', 'React Native, Visual Code, Git', 'Design pattern: Singleton, Observer', 'Structure: Redux'],
+            'url': ''
+        },
+        {
+            'cover': mibitel,
+            'title': 'MiBitel Peru',
+            'description': ['Customer: Viettel Peru', 'Java android, Android studio, Retrofit, Git', 'Design pattern: Singleton, Observer', 'Structure: MVP, MVVM'],
+            'url': 'https://apps.apple.com/pe/app/mi-bitel/id1172119104'
+        },
+        {
+            'cover': apec_finance,
+            'title': 'Apec Finance',
+            'description': ['Customer: Apec Group', 'React Native, Visual Code, Git', 'Design pattern: Singleton, Observer', 'Structure: Redux'],
+            'url': 'https://play.google.com/store/apps/details?id=com.apec.finance'
+        },
+        {
+            'cover': '',
+            'title': 'Paperless Shinhanbank',
+            'description': ['Customer: Shinhanbank', 'React Native, Visual Code, Git', 'Design pattern: Singleton, Observer', 'Structure: Redux'],
+            'url': ''
+        }
+    ]
+};
